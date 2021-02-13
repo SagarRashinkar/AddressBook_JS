5c18f2b50bbdd3600713dd104c6b459cdf0bb9c0
Sagar <sagarrashinkar2@gmail.com>
2021-02-13 16:23:42 +0530

    Added: Validation of contacts

--- AddressBook/AddressBook.js
+++ AddressBook/AddressBook.js
@@ -1,1 +1,20 @@
 const util = require('../Utility/Utility');
+const input = require('../Utility/Input');
+
+try {
+
+
+    let fName = input.getFirstName();
+    let lName = input.getLastName();
+    let address = input.getAddress();
+    let city = input.getCity();
+    let state = input.getState();
+    let zip = input.getZip();
+    let phone = input.getPhone();
+    let email = input.getEmail();
+
+    const data = new util(fName, lName, address, city, state, zip, phone, email);
+    console.log(data.toString());
+} catch (err) {
+    console.error(err);
+}
